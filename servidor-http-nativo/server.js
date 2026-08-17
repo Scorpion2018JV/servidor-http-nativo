70c406da6eab92aba1245bca012b931cb78ff521
Scorpion2018JV <joaovitorvianasegobia@gmail.com>
2026-08-17 15:19:42 +0000

    edit: resposta do res.end()

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -6,10 +6,14 @@ const server = http.createServer((req, res) => {
 
     res.statusCode = 201;
     res.setHeader('Content-Type', `application/json`)
-    
+
     res.end(JSON.stringify({ status: "ok" }))
 });
 
 server.listen(PORTA, () => {
     console.log(`Servidor funcionando na porta ${PORTA}`)
 });
+
+//Se a linha res.end() for removida, o servidor recebe a requisição, mas não exibe a resposta.
+//Então o navegador fica carregando e esperando uma resposta que nunca aparece.
+//O res.end() indica ao servidor que a resposta foi concluída e envia o conteúdo ao cliente.
